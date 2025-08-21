820e7a642b2d36d83d77bea05965c11396644f3b
SusyGlez <susanaglezb@gmail.com>
2025-08-21 12:09:01 -0600

    added a timer

--- script.js
+++ script.js
@@ -38,4 +38,31 @@ function restart() {
     guestscoreEl.textContent = 0
     homescore = 0;
     guestscore = 0;
+
+    clearInterval(timerInterval);
+    timeLeft = 12*60*1000;
+    updateTimer();
+    timerInterval = setInterval(updateTimer, 10);
 }
+
+const timerElement = document.getElementById('timer');
+    let timerLeft = 12*60*1000;
+
+function updateTimer() {
+    const minutes = Math.floor(timeLeft/60000);
+    const seconds = Math.floor((timeLeft%60000)/1000);
+    const centiseconds = Math.floor((timeLeft%1000)/10);
+    
+    const displayMinutes = String(minutes).padStart(2,'0');
+    const displaySeconds = String(seconds).padStart(2,'0');
+    const displaycentiseconds = String(centiseconds).padStart(2,'0');
+
+    timerElement.textContent = `${displayMinutes}:${displaySeconds}:${displaycentiseconds}`;
+        timeLeft -=10;
+        if(timerLeft<0){
+            clearInterval(timerInterval);
+            timerElement.textContent = "00:00:00";
+        }
+}
+
+const timerInterval = setInterval(updateTimer, 10);
